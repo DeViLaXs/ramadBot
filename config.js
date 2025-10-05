e5140698963069ba4be5d6b5e4d773dcd21c390d
omar <oamsa20@gmail.com>
2025-10-05 21:49:51 +0300

    fix bot

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
-module.exports = {
-  BOT_TOKEN: "7247147386:AAG-t_5VhJv7iEBU1jmtfhTVrPYd2LYcSO4"
-};
+// module.exports = {
+//   BOT_TOKEN: "7247147386:AAG-t_5VhJv7iEBU1jmtfhTVrPYd2LYcSO4"
+// };
